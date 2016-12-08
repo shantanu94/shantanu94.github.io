c8f7e3f40cd70dae5a95e98484f17d1beb275a5a
shantanu94 <bhardwaj.35@osu.edu>
2016-12-08 00:49:29 -0500

    trying something with nav brand

--- js/index.js
+++ js/index.js
@@ -4,3 +4,15 @@ $(document).ready(function(){
 		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
     	});
 });
+
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
